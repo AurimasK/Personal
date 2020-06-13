7febf533afbf91d760cee71ba9bcdac4135675ce
Aurimas Kancavicius <aurimask.94@gmail.com>
2020-06-13 12:14:09 +0300

    Add basic page design

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import avatar from './images/me.jpg';
+import mailIcon from './images/mail.svg';
+import linkedinIcon from './images/linkedin.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="background">
+      <div className="avatar-container">
+        <img alt="Me" src={avatar} />
+      </div>
+      <h1>Hello, my name is Aurimas.</h1>
+      <p>I&apos;m a fullstack developer. </p>
+      <p>I&apos;m proficient with Ruby on Rails.</p>
+      <p>
+        Right now I&apos;m improving my MERN stack skils:
+        MongoDB/Express/React/Node
+      </p>
+      <p>You can reach me with Email or on LinkedIn:</p>
+      <div className="icons">
+        <a href="mailto:aurimas@kancavicius.lt">
+          <img alt="Email" src={mailIcon} />
+        </a>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/aurimaskancavicius/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <img alt="LinkedIn" src={linkedinIcon} />
         </a>
-      </header>
+      </div>
     </div>
   );
 }
